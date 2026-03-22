6ada19681cb1f1afaeb8644fa7bfc8aa9f330d31
cieldoit <rexilldator@gmail.com>
2026-03-22 13:31:35 +0800

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://csu-lost-found-api.onrender.com"
 });
 
 // Add a request interceptor to include the auth token in every request
