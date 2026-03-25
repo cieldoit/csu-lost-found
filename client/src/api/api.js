a01ab02478e7330dee3e89900b2d848e6e82d269
cieldoit <rexilldator@gmail.com>
2026-03-25 10:27:15 +0800

    kulang ug api HAHAHA

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://csu-lost-found-api.onrender.com"
+  baseURL: "https://csu-lost-found-api.onrender.com/api"
 });
 
 // Add a request interceptor to include the auth token in every request
