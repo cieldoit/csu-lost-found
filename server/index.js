cf768a848bb3656b56c1253b45664e549a87ae96
cieldoit <rexilldator@gmail.com>
2026-03-25 10:01:13 +0800

    health-cron-job

--- server/index.js
+++ server/index.js
@@ -53,3 +53,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Server is awake!');
+});
