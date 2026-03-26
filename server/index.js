8fc05d01ee2f52b73673d7a44320ae6ee401b48c
cieldoit <rexilldator@gmail.com>
2026-03-26 13:34:39 +0800

    fix double lsiten and make trust the proxy

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const connectDB = require("./config/db");
 connectDB();
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(cors());
 app.use(helmet());
@@ -59,9 +60,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
